6ad5aa1135627b056074c341e181d7341ff8c057
Jacob Holz <jakeholz03@gmail.com>
2024-04-01 15:41:46 -0500

    easy peasy

--- proj3/MapReduce.java
+++ proj3/MapReduce.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
 import java.util.StringTokenizer;
 
+import javax.naming.Context;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -14,20 +16,65 @@ import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
 public class MapReduce {
 
+  private static final int IDENT = 0;
+  private static final int TYPE = 1;
+  private static final int NAME = 2;
+  private static final int YEAR = 3;
+  private static final int RATING = 4;
+  private static final int GENRE_LIST = 5;
+
+  private static final String MOVIE = "movie";
+  private static final String RATING_CUTOFF = "7.5";
+  private static final int FIRST_YEAR = 1991;
+  private static final int LAST_YEAR = 2020;
+  private static final int YEARS_IN_A_RANGE = 10;
+
+  private static final String GENRE1A = "Action";
+  private static final String GENRE1B = "Thriller";
+  private static final String GENRE2A = "Adventure";
+  private static final String GENRE2B = "Drama";
+  private static final String GENRE3A = "Comedy";
+  private static final String GENRE3B = "Romance";
+
   public static class TokenizerMapper
       extends Mapper<Object, Text, Text, IntWritable> {
 
     private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
+    private Text category = new Text();
 
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
       FileSplit fileSplit = (FileSplit) context.getInputSplit();
       String filename = fileSplit.getPath().getName();
 
-      StringTokenizer itr = new StringTokenizer(value.toString());
+      StringTokenizer itr = new StringTokenizer(value.toString(), "\n");
       while (itr.hasMoreTokens()) {
-        word.set(itr.nextToken());
-        context.write(word, one);
+        String[] line = itr.nextToken().split(";");
+        if (line.length <= GENRE_LIST || !line[TYPE].equals(MOVIE) || line[RATING].compareTo(RATING_CUTOFF) < 0) {
+          break;
+        }
+        int year = 0;
+        try {
+          year = Integer.parseInt(line[YEAR]);
+        } catch (Exception e) {}
+        if (year < FIRST_YEAR || year > LAST_YEAR) {
+          break;
+        }
+        int rangeStart = year - (year-1) % YEARS_IN_A_RANGE;
+        int rangeEnd = rangeStart + 9;
+        String yearRange = "[" + rangeStart + "-" + rangeEnd + "]";
+
+        if (line[GENRE_LIST].contains(GENRE1A) && line[GENRE_LIST].contains(GENRE1B)) {
+          category.set(yearRange + "," + GENRE1A + ";" + GENRE1B);
+          context.write(category, one);
+        }
+        if (line[GENRE_LIST].contains(GENRE2A) && line[GENRE_LIST].contains(GENRE2B)) {
+          category.set(yearRange + "," + GENRE2A + ";" + GENRE2B);
+          context.write(category, one);
+        }
+        if (line[GENRE_LIST].contains(GENRE3A) && line[GENRE_LIST].contains(GENRE3B)) {
+          category.set(yearRange + "," + GENRE3A + ";" + GENRE3B);
+          context.write(category, one);
+        }
       }
     }
   }
